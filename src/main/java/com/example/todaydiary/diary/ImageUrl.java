1508312985025458266e656f7573d68c3d08e2c5
Dean404 <97504973+Dean404@users.noreply.github.com>
2022-02-16 15:22:48 +0900

    재커밋.

---
+++ src/main/java/com/example/todaydiary/diary/ImageUrl.java
@@ -0,0 +1,31 @@
+//package com.example.todaydiary.diary;
+//
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//
+//public class ImageUrl {
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Id
+//    private Long id;
+//
+//    @Column(nullable = false)
+//    private String imageUrl;
+//
+//    @Builder
+//    public ImageUrl(String imageUrl){
+//
+//        this.imageUrl = imageUrl;
+//    }
+//
+//}
+//
+//
+//
